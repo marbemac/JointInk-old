a80a21fbbe279c47ef5348b7251a48a22d567729
Marc MacLeod <marbemac@gmail.com>
2013-02-14 13:54:37 -0500

    http streaming support. still need to convert to slim. only on homepage for now.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,19 +1,13 @@
 class PagesController < ApplicationController
   layout 'fullscreen_bg', :only => [:home]
+  stream
 
   def home
     @fullscreen = true
     if signed_in?
       redirect_to root_url(:subdomain => current_user.username)
     end
-  end
-
-  def about
-    @title = 'About'
-  end
-
-  def faq
-    @title = 'FAQ'
+    render :stream => true
   end
 
 end
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,2 +1,5 @@
 worker_processes 3
 timeout 30
+preload_app true
+port = ENV["PORT"].to_i
+listen port, :tcp_nopush => false
