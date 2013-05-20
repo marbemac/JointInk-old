e38a3d5ec4c648d876f54a25f4b843a8a19b4196
Marc MacLeod <marbemac@gmail.com>
2013-05-20 18:39:36 -0400

    recommendations should not require login to view
    Signed-off-by: Marc MacLeod <marbemac@gmail.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_filter :authenticate_user!, :except => [:index,:show,:channels,:check_username]
+  before_filter :authenticate_user!, :except => [:index,:show,:channels,:check_username,:recommendations]
 
   def index
 
