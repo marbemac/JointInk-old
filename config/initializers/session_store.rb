b56ce081db6115c881b036deb1015b435804aa6a
Marc MacLeod <marbemac@gmail.com>
2013-05-24 20:20:11 -0400

    fix
    Signed-off-by: Marc MacLeod <marbemac@gmail.com>

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,10 +1,10 @@
 # Be sure to restart your server when you modify this file.
 
-#if Rails.env == 'production'
-#  JointInk::Application.config.session_store ActionDispatch::Session::CacheStore, :expire_after => 30.minutes
-#else
-JointInk::Application.config.session_store :cookie_store, key: '_JointInk_session', :domain => :all
-#end
+if Rails.env == 'production'
+  JointInk::Application.config.session_store ActionDispatch::Session::CacheStore, :expire_after => 30.minutes
+else
+  JointInk::Application.config.session_store :cookie_store, key: '_JointInk_session', :domain => :all
+end
 
 
 # Use the database for sessions instead of the cookie-based default,
