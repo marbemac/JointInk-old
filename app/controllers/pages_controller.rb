56409464d6ab9b1467fbf9886d5d4b2b5089d63f
Marc MacLeod <marbemac@gmail.com>
2013-05-25 23:20:26 -0400

    quicky
    Signed-off-by: Marc MacLeod <marbemac@gmail.com>

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,10 +3,11 @@ class PagesController < ApplicationController
   caches_action :home, if: lambda { !signed_in? }
 
   def home
+    expires_in 3.hours, :public => true, 'max-stale' => 0
+
     if signed_in?
       redirect_to root_url(:subdomain => current_user.username)
     else
-      expires_in 3.hours, :public => true, 'max-stale' => 0
       @fullscreen = true
       @channels = Channel.active.order("posts_count DESC").limit(3)
       render
