05b011a918c1f767ed57cd26556faad627b391af
Marc MacLeod <marbemac@gmail.com>
2013-06-13 19:11:51 -0700

    quick fix
    Signed-off-by: Marc MacLeod <marbemac@gmail.com>

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -7,7 +7,7 @@ class EmailsController < ApplicationController
       last_post = user.posts.order('created_at DESC').first
 
       # don't allow posts within 60 seconds
-      unless Time.now - last_post.created_at.to_i > 60
+      unless Time.now.to_i - last_post.created_at.to_i > 60
 
         title = params['subject']
 
