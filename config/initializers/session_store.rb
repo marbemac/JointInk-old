93dae32e78615335566ce6c2d3864b3017c12a08
Marc MacLeod <marbemac@gmail.com>
2013-07-30 12:35:02 -0700

    fix cookie issue
    Signed-off-by: Marc MacLeod <marbemac@gmail.com>

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-JointInk::Application.config.session_store :cookie_store, key: '_JointInk_session', :domain => :all
+JointInk::Application.config.session_store :cookie_store, key: '_JointInk_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
