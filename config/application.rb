0737a4fb55c0b8d2cf89e912a5093c40b221d1cc
Marc MacLeod <marbemac@gmail.com>
2013-07-30 12:24:47 -0700

    update app file
    Signed-off-by: Marc MacLeod <marbemac@gmail.com>

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(:default, Rails.env)
 
 module JointInk
   class Application < Rails::Application
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -20,13 +21,26 @@ module JointInk
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    # Use SQL instead of Active Record's schema dumper when creating the database.
+    # This is necessary if your schema can't be completely dumped by the schema dumper,
+    # like if you have constraints or database-specific column types
+    #config.active_record.schema_format = :sql
+
+    # Enforce whitelist mode for mass assignment.
+    # This will create an empty whitelist of attributes available for mass-assignment for all models
+    # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
+    # parameters by using an attr_accessible or attr_protected declaration.
+    # config.active_record.whitelist_attributes = true
+
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
     config.autoload_paths += %W(#{config.root}/lib)
 
-    config.assets.precompile += ['errors.css','editor.js','editor.css','redactor.js','redactor.css','modernizr.js','splash_page.js']
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+    config.assets.precompile += %w(errors.css editor.js editor.css redactor.js redactor.css splash_page.js .svg .eot .woff .ttf)
 
     # pjax
     config.middleware.use Rack::Pjax
+
   end
 end
